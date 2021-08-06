cf65df037fc4086078dacc4fb9d8188e6a2bdf99
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2021-08-06 11:35:44 +0300

    Update only dedicated submodule

--- checkout.sh
+++ checkout.sh
@@ -9,6 +9,6 @@ git clone https://github.com/tezedge/tezedge-fuzzing.git "$dir"
 cd $dir
 git config -f .gitmodules submodule.code/tezedge.url "$url"
 git config -f .gitmodules submodule.code/tezedge.branch "$branch"
-git submodule update --init --recursive --remote
+git submodule update --init --recursive --remote code/tezedge
 cd code/tezedge
 git status
